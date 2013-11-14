288a342857d2e9fb1986fcab9e00999008047668
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2013-11-14 21:01:39 +0800

    Add docs

--- src/strict_typed_ops/strict.clj
+++ src/strict_typed_ops/strict.clj
@@ -3,18 +3,25 @@
   (:require [clojure.core.typed :as t]))
 
 (defmacro vector>
+  "Make a vector of element type t. During type checking, arguments must be of type t."
   [t & args]
   `(t/ann-form (clojure.core/vector ~@args) (t/Vec ~t)))
 
 (defmacro hash-map>
+  "Make a hash map with keys type ks and vals type vs. During type checking,
+  argument keys and vals must be of type ks and vs respectively."
   [ks vs & args]
   `(t/ann-form (clojure.core/hash-map ~@args) (t/Map ~ks ~vs)))
 
 (defmacro hash-set>
+  "Make a hash set of element type t. During type checking, arguments must be
+  of type t."
   [t & args]
   `(t/ann-form (clojure.core/hash-set ~@args) (t/Set ~t)))
 
 (defmacro sorted-set>
+  "Make a sorted set of element type t. During type checking, arguments must be
+  of type t."
   [t & args]
   `(t/ann-form (clojure.core/sorted-set ~@args) (t/SortedSet ~t)))
 
@@ -22,7 +29,10 @@
      (All [x
            [y :< x]]
           [(t/Vec x) y y * -> (t/Vec x)]))
-(defn conj-vec [target arg & args]
+(defn conj-vec 
+  "conj onto a vector. During type checking, the candiates to conj must have the
+  same type as the target element type."
+  [target arg & args]
   (apply conj target arg args))
 
 (t/ann ^:no-check conj-map
@@ -30,14 +40,20 @@
            [k1 :< k]
            [v1 :< v]]
           [(t/Map k v) (U nil '[k1 v1]) (U nil '[k1 v1]) * -> (t/Map k v)]))
-(defn conj-map [target arg & args]
+(defn conj-map 
+  "conj onto a map. During type checking, the arguments to conj must have the
+  same type as the target element type."
+  [target arg & args]
   (apply conj target arg args))
 
 (t/ann ^:no-check conj-set
        (All [x
              [y :< x]]
             [(t/Set x) y y * -> (t/Set x)]))
-(defn conj-set [target arg & args]
+(defn conj-set 
+  "conj onto a set During type checking, the arguments to conj must have the
+  same type as the target element type."
+  [target arg & args]
   (apply conj target arg args))
 
 (t/ann ^:no-check into-map
@@ -45,21 +61,30 @@
            [k1 :< k]
            [v1 :< v]]
           [(t/Map k v) (U nil (t/Seqable '[k1 v1])) -> (t/Map k v)]))
-(defn into-map [target arg]
+(defn into-map 
+  "Add items from a collection into a map. During type checking, the second argument must have the
+  same type element type as the target element type."
+  [target arg]
   (into target arg))
 
 (t/ann ^:no-check into-set
      (All [x
            [y :< x]]
           [(t/Set x) (U nil (t/Seqable y)) -> (t/Set x)]))
-(defn into-set [target arg]
+(defn into-set 
+  "Add items from a collection into a set. During type checking, the second argument must have the
+  same type element type as the target element type."
+  [target arg]
   (into target arg))
 
 (t/ann ^:no-check into-sorted-set
      (All [x
            [y :< x]]
           [(t/SortedSet x) (U nil (t/Seqable y)) -> (t/SortedSet x)]))
-(defn into-sorted-set [target arg]
+(defn into-sorted-set 
+  "Add items from a collection into a sorted set. During type checking, the second argument must have the
+  same type element type as the target element type."
+  [target arg]
   (into target arg))
 
 
@@ -89,4 +114,3 @@
   ; can't add (U Long Double) to (SortedSet Long)
   (t/cf (into-sorted-set (sorted-set> Long) [1 2 2.2]))
   )
-
