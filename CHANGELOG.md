1c08d9226aa952d2e35bd5ddd540f1a081796cee
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2013-11-14 21:35:11 +0800

    remove from gh-pages

--- CHANGELOG.md
+++
@@ -1,3 +0,0 @@
-0.1.0 - Released 14 November 2013
-
-- constructor, `conj` and `into` wrappers for maps, sets and sorted sets
--- test/strict_typed_ops/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns strict-typed-ops.core-test
-  (:use clojure.test
-        strict-typed-ops.core))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
